e865f36c528323d515853339ab88bbf8c47652c9
aashirbadb <aashirbadbhandari@gmai.comclear>
2024-07-12 20:32:07 +0545

    Initial commit

---
+++ scrape.py
@@ -0,0 +1,44 @@
+from bs4 import BeautifulSoup
+import requests
+from feedgen.feed import FeedGenerator
+from datetime import datetime
+from zoneinfo import ZoneInfo
+
+URL = "http://exam.ioe.edu.np"
+DATE_FORMAT = "%A, %B %m, %Y"
+TIMEZONE = ZoneInfo("Asia/Kathmandu")
+
+def parse_date(date):
+    return datetime.strptime(date, DATE_FORMAT).astimezone(TIMEZONE)
+
+
+
+res = requests.get(URL)
+if res.ok:
+    soup = BeautifulSoup(res.text, "html.parser")
+    table = soup.find(id = "datatable")
+    rows = table.find_all("tr")
+
+    fg = FeedGenerator()
+    fg.id(URL)
+    fg.title('IOE Exam Website RSS Feed')
+    fg.link( href=URL )
+    fg.logo(URL+"/favicon.ico")
+    fg.description("Scrapes exam.ioe.edu.np and generates RSS feed of notices")
+
+    for row in map(lambda r: r.find_all("td"), rows[1:]):
+        item = fg.add_entry(order="append")
+
+        item.title(row[1].get_text().strip())
+
+        id = row[3].find("a").get("href").split("/")[-1]
+        item.id(id)
+
+        date = datetime.strptime(row[2].get_text().strip(), DATE_FORMAT).astimezone(TIMEZONE)
+        item.published(date)
+
+        link = URL + row[1].find("a").get("href")
+        item.link(href=link)
+
+    fg.rss_file('rss.xml', pretty=True)
+    fg.atom_file('atom.xml', pretty=True)
